f341a23423276e43924764d0eac6d7fe23065703
WinZ606 <2005bnm@gmail.com>
2024-10-17 08:49:55 +0200

    1. 2. feladat megoldva

---
+++ feladatok.py
@@ -0,0 +1,27 @@
+def feladat1(lista):
+    szamok = 0
+    i = 0
+    while (i<len(lista)):
+        if lista[i] > 0:
+            szamok += 1
+        i += 1
+    return szamok
+
+def feladat2(lista):
+    szamok = 0
+    i = 0
+    while (i<len(lista)):
+        if lista[i] < 0:
+            szamok += lista[i]
+        i += 1
+    return szamok
+
+def feladat3(lista):
+    szamok = 0
+    i = 0
+    while (i<len(lista)):
+        if lista[i] % 5 == 0:
+            szamok += lista[i]
+            szamok / len(lista[i])
+        i += 1
+    return szamok
---
+++ main.py
@@ -0,0 +1,5 @@
+import feladatok
+
+print(f"1. feladat\n{feladatok.feladat1([12,21,56,32,-5,-23,35])}")
+print(f"2. feladat\n{feladatok.feladat2([12,21,56,32,-5,-23,35])}")
+print(f"3. feladat\n{feladatok.feladat3([12,21,56,32,-5,-23,35])}")
